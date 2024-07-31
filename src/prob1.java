5ba4b99bd0a88201e7e7c666757d318688c669d7
jdm2131 <jdm2131@naver.com>
2024-07-31 10:06:43 +0900

    Algorithm

--- src/prob1.java
+++ src/prob1.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class prob1 {
+public class prob1 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String answer = solution.solution(4);
+        System.out.println("answer = " + answer);
+    }
+
+    public static class Solution{
+        public String solution(int num) {
+            String answer=null;
+            if (num % 2 == 0) {
+                answer = "EVEN";
+            } else {
+                answer = "ODD";
+            }
+            return answer;
+        }
+    }
 }
